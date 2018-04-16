fa6f2d5667f27a953a6fa60ad5a7190b12020aa1
Jeffrey Lee <jjlee16@gmail.com>
2018-04-16 18:48:30 +0000

    finished step 8

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var secret = require('./secrets.js');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -21,8 +22,25 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+function downloadImageByURL(url, filePath) {
+  request.get(url,).on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log(result);
+getRepoContributors("jquery", "jquery", function(err, results) {
+  for (result in results){
+    //console.log(results[result]);
+    //console.log(result);
+    downloadImageByURL(results[result], "./avatar" + result + ".jpg");
+  }
+
 });
+
+//downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg");
